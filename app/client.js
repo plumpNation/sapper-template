772767cfdefca7f83f405b8e04bb8b186dbc498f
Gavin King <gavin.king@itslearning.com>
2018-07-23 15:33:13 +0200

    Initial commit

--- app/client.js
+++ app/client.js
@@ -2,6 +2,6 @@ import { init } from 'sapper/runtime.js';
 import { manifest } from './manifest/client.js';
 
 init({
-	target: document.querySelector('#sapper'),
-	manifest
+  target: document.querySelector('#sapper'),
+  manifest
 });
--- app/server.js
+++ app/server.js
@@ -5,12 +5,12 @@ import compression from 'compression';
 import { manifest } from './manifest/server.js';
 
 polka() // You can also use Express
-	.use(
-		compression({ threshold: 0 }),
-		sirv('assets'),
-		sapper({ manifest })
-	)
-	.listen(process.env.PORT)
-	.catch(err => {
-		console.log('error', err);
-	})
+  .use(
+    compression({ threshold: 0 }),
+    sirv('assets'),
+    sapper({ manifest })
+  )
+  .listen(process.env.PORT)
+  .catch(err => {
+    console.log('error', err);
+  })
